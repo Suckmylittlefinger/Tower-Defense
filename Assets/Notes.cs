e9bba3dcfc6dcedf89df430ada00a73c5b56d79c
EricLamoureux <62370299+Suckmylittlefinger@users.noreply.github.com>
2024-11-10 11:02:09 -0500

    Enemies and Notes

---
+++ Assets/Notes.cs
@@ -0,0 +1,8 @@
+
+//THINGS TO ADD / WORK ON
+
+// Need to add losing to game
+// Tank enemies not fully working yet
+// Only basic enemies should spawn in first level
+
+//Maybe add consumable traps
