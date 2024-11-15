0de2c858fcd8e9aa3f9900581edcaf77e6def5d1
EricLamoureux <62370299+Suckmylittlefinger@users.noreply.github.com>
2024-11-15 09:21:24 -0500

    Fixed the trees

--- Assets/Notes.cs
+++ Assets/Notes.cs
@@ -2,6 +2,10 @@
 //THINGS TO ADD / WORK ON
 
 // Enemies left alive not always accurate. Need to debug
+// Wave does not start if game RESTARTED from menu
+
 // Combine sprites into a texture sheet
+// Balance the game
+
 
 //Maybe add consumable traps
